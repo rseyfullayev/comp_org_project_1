0bb63bdd53f7ae5b3693eb7913c9d10293c61ab3
itu-itis24-seyfullayev24 <seyfullayev24@itu.edu.tr>
2026-03-21 17:25:39 +0300

    edited ALU and added flagregister; half done

--- src/ArithmeticLogicUnit.v
+++ src/ArithmeticLogicUnit.v
@@ -1,39 +1,58 @@
 module flagReg (
-    input wire flagIn
-    input wire WF,
 
-    output reg Cin
+    input wire [3:0] flagIn,
+    input wire [3:0] WF,
+    input wire clk,
+
+    output wire Cin
 );
+
+    reg [3:0] flag;
+
+    always @(posedge clk) begin
+        flag <= (flag & ~WF) | (flagIn & WF); // Using WF as a mask.
+    end
+
+    assign Cin = flag[2];
 
 endmodule
 
 
-
-
 module ALU(
-    input wire [15:0] input1,
-    input wire [15:0] input2,
+    input wire [15:0] A,
+    input wire [15:0] B,
+    input wire clk,
 
     input wire [3:0] Funsel,
     input wire Cin,
 
-    output reg [15:0] out,
-    output reg [3:0] flagOut
+    output reg [3:0] WF,
+    output reg [3:0] flagOut,
+    output reg [15:0] ALUOut,
 );
 
 
 always @(posedge clk) begin
-    case Funsel
-        2'b0000:
-            flagOut <= A;
-        2'b0001
-            flagOut <= B;
+    case (Funsel)
+        4'b0000:
+            ALUOut <= A;
+            WF <= 2'1010;
+        4'b0001:
+            ALUOu <= B;
+        4'b0010:
+            ALUOut <= ~A;
+        4'b0011:
+            ALUOut <= ~B;
+        4'b0100:
+            ALUOut <= A+B;
+
+        4'b0101:
+
+        4'b0110:
+
+        4'b0010:
 
     endcase
-end
-
-always @(*) begin
-
 end
 
 
